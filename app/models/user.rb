5e9da3bcc9c90f0f8aed6f67b82bbbbec4931892
Mihir Buch <mihirjb@gmail.com>
2014-12-17 14:52:00 +0800

    removed firstname validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,5 @@ class User < ActiveRecord::Base
           has_many :listings, :dependent =>:destroy
           has_one :profile, dependent: :destroy, autosave: true
           accepts_nested_attributes_for :profile
-          
-          validates :firstname, presence: true
-          
+                    
 end
